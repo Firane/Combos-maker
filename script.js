59d205afe2c3755383330ebfaa65df49f4ad48ee
Firane <gael.querharo@gmail.com>
2023-08-26 03:39:06 +0200

    starting the combo editor and displayer

--- script.js
+++ script.js
@@ -18,7 +18,16 @@ const characters = [
   "DeeJay",
   "Honda",
 ];
+let edit;
+const comboContainer = Array.from(
+  document.querySelectorAll(".combos__display__list__comboContainer__oneCombo")
+);
 
+const edits = Array.from(document.querySelectorAll(".edit"));
+
+const input = Array.from(
+  document.querySelectorAll(".combos__display__editor__inputsList__input")
+);
 const navArrow = document.querySelectorAll(".fa-caret-left");
 // Ca gere le bouton du menu de nav
 
@@ -60,11 +69,49 @@ const liGenerator = () => {
   });
 };
 
+const listenInputs = () => {
+  input.map((element) => {
+    element.addEventListener("click", (e) => {
+      let theClone = e.target.firstElementChild.cloneNode(true);
+      let theDiv = edit.getElementsByTagName("div")[1];
+      console.log(theDiv);
+      console.log(theClone);
+      theDiv.appendChild(theClone);
+
+      // ca ajoute une balise img au lieu de la div contenant limg ca pause soucis plus tard pour le add event listener
+    });
+  });
+};
+
+const listenEdits = () => {
+  edits.map((element) => {
+    element.addEventListener("click", (e) => {
+      edit = e.target.parentElement.parentElement;
+      console.log(edit);
+      console.log(edit.getElementsByTagName("div")[1]);
+    });
+  });
+};
+
+const listenComboContainer = () => {
+  comboContainer.map((element) => {
+    element.addEventListener("click", (e) => {
+      console.log(e);
+      if (e.target.tagName === "IMG") {
+        console.log("saucisse");
+      }
+    });
+  });
+};
+
 scroller.addEventListener("click", (e) => {
   navShow();
 });
 
 window.addEventListener("load", () => {
   liGenerator();
+  listenInputs();
+  listenEdits();
+  listenComboContainer();
   charactersListContainer.classList.add("addBorder");
 });
